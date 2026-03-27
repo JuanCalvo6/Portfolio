d36ed3e53df12420ab781202ec8ab6d56fb4f8fb
Juan Calvó <juanfc992@gmail.com>
2026-03-27 15:54:07 -0300

    Primer commit - proyecto cv sprinboot

---
+++ src/test/java/com/juanCalvo6/cv_springboot/CvSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanCalvo6.cv_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
