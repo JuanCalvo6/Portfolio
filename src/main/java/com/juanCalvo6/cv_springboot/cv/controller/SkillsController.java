87a85b520efede26a03ca155e4eca25d6bbd8f06
Juan Calvó <juanfc992@gmail.com>
2026-04-10 16:15:12 -0300

    Se agrega un filtro de busqueda para las skills utilizando RequestParam

--- src/main/java/com/juanCalvo6/cv_springboot/cv/controller/SkillsController.java
+++ src/main/java/com/juanCalvo6/cv_springboot/cv/controller/SkillsController.java
@@ -3,10 +3,7 @@ package com.juanCalvo6.cv_springboot.cv.controller;
 import com.juanCalvo6.cv_springboot.cv.model.Skill;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,9 +14,19 @@ public class SkillsController {
 
     private final List<Skill> skills = new ArrayList<>();
 
+//    @GetMapping
+//    public String showSkills(Model model){
+//        model.addAttribute("skills", skills);
+//        return "skills";
+//    }
+
     @GetMapping
-    public String showSkills(Model model){
-        model.addAttribute("skills", skills);
+    public String showSkills(@RequestParam(defaultValue = "", required = false) String filter, Model model){
+        List<Skill> skillsFilter = skills.stream()
+                        .filter(skill -> skill.getName().toLowerCase().contains(filter.toLowerCase()))
+                        .toList();
+        model.addAttribute("skills", skillsFilter);
+        model.addAttribute("filter", filter);
         return "skills";
     }
 
